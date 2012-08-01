8714dae25b228b1c3cada8da4c0debe9702ee013
nico401 <nico401.lassaux@hotmail.fr>
2012-08-01 15:52:35 +0200

    Fix some html validation errors (W3C)

--- Penelope/settings.py
+++ Penelope/settings.py
@@ -98,7 +98,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,11 +116,11 @@ WSGI_APPLICATION = 'Penelope.wsgi.application'
 TEMPLATE_DIRS = (
     # Apps must be before the Penelope to overwrite template (search in order)
     'Penelope/templates',
-    '/Users/Nicolas/Virtualenvs/LaBRI/lib/python2.7/site-packages/debug_toolbar/templates',
+    # '/Users/Nicolas/Virtualenvs/LaBRI/lib/python2.7/site-packages/debug_toolbar/templates',
 )
 
 INSTALLED_APPS = (
-    'debug_toolbar',
+    # 'debug_toolbar',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
